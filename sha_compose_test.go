604e54e1321a3dde7d4348711a7671720571841b
Evgeniy Sokolov <ewgraf@gmail.com>
2016-02-02 00:23:34 +0100

    ShaIn and ShaOut compose and tests

--- sha_compose_test.go
+++ sha_compose_test.go
@@ -4,20 +4,45 @@ import (
 	"testing"
 )
 
-func TestShaCompose(t *testing.T) {
+func TestShaInCompose(t *testing.T) {
 	expected := "F4CC376CD7A834D997B91598FA747825A238BE0A"
 
 	params := map[string]string{
-		"lANGUAGE": "en_US",
-		"AMOUnT": "1500",
-		"CURReNCY": "EUR",
-		"ORDERID": "1234",
-		"PSPID": "MyPSPID",
+		"lANGUAGE":    "en_US",
+		"NOT_ALLOWED": "foo",
+		"AMOUnT":      "1500",
+		"CURReNCY":    "EUR",
+		"ORDERID":     "1234",
+		"PSPID":       "MyPSPID",
 	}
 
-	sign := ShaCompose(params, "Mysecretsig1875!?")
+	sign := ShaInCompose(params, "Mysecretsig1875!?")
 
-	if (sign != expected) {
-		t.Fatalf("Sha compose failed, expected %s, got %s", expected, sign)
+	if sign != expected {
+		t.Fatalf("ShaIn compose failed, expected %s, got %s", expected, sign)
+	}
+}
+
+func TestShaOutCompose(t *testing.T) {
+	expected := "209113288F93A9AB8E474EA78D899AFDBB874355"
+
+	params := map[string]string{
+		"amount":      "15",
+		"BRAND":       "VISA",
+		"ACCEPTANCE":  "1234",
+		"CARDNO":      "XXXXXXXXXXXX1111",
+		"NOT_ALLOWED": "foo",
+		"currency":    "EUR",
+		"NCERROR":     "0",
+		"orderID":     "12",
+		"PAYID":       "32100123",
+		"PM":          "CreditCard",
+		"STATUS":      "9",
+	}
+
+	sign := ShaOutCompose(params, "Mysecretsig1875!?")
+
+	if sign != expected {
+		t.Fatalf("ShaOut compose failed, expected %s, got %s", expected, sign)
 	}
 }
