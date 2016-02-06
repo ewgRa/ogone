2fcc5b649e635eb15a29ca12d129b175c934c267
Evgeniy Sokolov <ewgraf@gmail.com>
2016-02-06 22:53:57 +0100

    fileless fmt check

--- files/go-fmt-changed-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git diff HEAD^ --name-status | grep "^D" -v | sed 's/^.\t//g' | grep "\.go$" > changed_files
-
-while read FILE; do
-    go fmt $FILE
-done < changed_files
-
-rm changed_files
